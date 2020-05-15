df897477b04d94b7a0ee5b5c0104a7888ee86000
Oleg.Trohov <lejick@gmail.com>
2020-05-15 16:24:52 +0300

    .

--- nnfs-p/p5.py
+++ nnfs-p/p5.py
@@ -1,12 +1,16 @@
 import numpy as np
 import nnfs
+from nnfs.datasets import spiral_data
 
 nnfs.init()
 
 data = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+        [2.0, 5.0, -1.0, 2.0],
+        [-1.5, 2.7, 3.3, -0.8]]
 X = np.array(data, dtype=np.float32)
+
+
+X, y = spiral_data(100, 3)
 
 
 class Layer_Dense:
@@ -22,8 +26,10 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+
+layer1 = Layer_Dense(2, 5)
+activation1=Activation_ReLU()
 layer1.forward(X)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+
+print(activation1.output)
