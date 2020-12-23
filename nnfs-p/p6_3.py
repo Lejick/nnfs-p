03005df1552063d272207c4a7615945383468016
Lejick <lejick@gmail.com.com>
2020-12-23 17:45:29 +0300

    .

--- nnfs-p/p6_3.py
+++ nnfs-p/p6_3.py
@@ -26,9 +26,20 @@ class Activation_ReLU:
         self.output = np.maximum(0, inputs)
 
 
-layer1 = Layer_Dense(2, 5)
-activation1 = Activation_ReLU()
-layer1.forward(X)
-activation1.forward(layer1.output)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
-print(activation1.output)
+
+X, y = spiral_data(100,3)
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+print(activation2.output[:5])
